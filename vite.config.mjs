0ed587feb39057fd38f7712f64fc12bd64637e5e
EATRIO <andrea.consulenze.hosting@proton.me>
2025-09-03 22:19:12 +0200

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tagger from "@dhiwise/component-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  plugins: [react()],
+  // Assicurati che NON ci sia `root: 'src'` o simili.
   build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000,
-  },
-  plugins: [tsconfigPaths(), react(), tagger()],
-  server: {
-    port: "4028",
-    host: "0.0.0.0",
-    strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+    outDir: 'dist',   // <— importantissimo
+    emptyOutDir: true
   }
-});
+})
